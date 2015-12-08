b59977f87b1660694534818a81d48f9cedf1582f
chan <chan.sisowath@gmail.com>
2015-12-08 11:21:31 +0800

    Update validation.js

--- priv/validation.js
+++ priv/validation.js
@@ -1,9 +1,6 @@
 // Semantic form Validation
 function validateSemanticForm(form,list) {
   var $form = $('#'+form); 
-  //console.log($form.form('get values',list));
-  if (form == 'startBid') return $form.form('get values', list); 
-  	else if ($form.form('is valid')) return $form.form('get values', list);
+  if ($form.form('is valid')) return $form.form('get values', list);
   return false;
-  //return $form.form('get values',list);
 }
